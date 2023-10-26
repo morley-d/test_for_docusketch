536e31f0454676d797cbcf0266169068ed93feec
San <andreal87@mail.ru>
2023-10-26 09:38:25 +0600

    refactor connect db

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,9 @@ from flask import Flask, request, jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
-client = MongoClient('mongodb://localhost:27017/')
 
-db = client['my_database']
-collection = db['my_collection']
+client = MongoClient('mongodb://user_name:user_password@mongodb:27017/mydb', authSource="admin")
+db = client.mydb
 
 
 @app.route('/', methods=['GET'])
@@ -20,11 +19,12 @@ def create():
     value = data.get('value')
 
     if key and value:
-        existing_data = collection.find_one({'key': key})
+        # existing_data = collection.find_one({'key': key})
+        existing_data = db.testdb.find_one({'key': key})
         if existing_data:
             return jsonify({'message': 'Key already exists'}), 400
         else:
-            collection.insert_one({'key': key, 'value': value})
+            db.testdb.insert_one({'key': key, 'value': value})
             return jsonify({'message': 'Data created'}), 201
     else:
         return jsonify({'message': 'Invalid data'}), 400
@@ -36,9 +36,10 @@ def update(key):
     new_value = data.get('new_value')
 
     if key and new_value:
-        existing_data = collection.find_one({'key': key})
+        # existing_data = collection.find_one({'key': key})
+        existing_data = db.testdb.find_one({'key': key})
         if existing_data:
-            collection.update_one({'key': key}, {'$set': {'value': new_value}})
+            db.testdb.update_one({'key': key}, {'$set': {'value': new_value}})
             return jsonify({'message': 'Data updated'}), 200
         else:
             return jsonify({'message': 'Key not found'}), 404
@@ -48,7 +49,7 @@ def update(key):
 
 @app.route('/read/<key>', methods=['GET'])
 def read(key):
-    data = collection.find_one({'key': key})
+    data = db.testdb.find_one({'key': key})
 
     if data:
         return jsonify({'key': data['key'], 'value': data['value']}), 200
